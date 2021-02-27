34d6cca3a514fd03c845f8358b1817b3ea4616e6
StanZ-rep <77188682+StanZ-rep@users.noreply.github.com>
2021-02-27 14:45:33 +0100

    second

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const functions = require("./functions")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path")
 app.set("view engine","hbs");
 app.use("/assets",express.static(path.join(__dirname,"./assets/css"))); 
